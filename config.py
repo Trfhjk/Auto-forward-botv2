8f41a4bd9614c16ae037ad285f95ca1d91f38932
Sushilgithubbot <sushilgithubbot@gmail.com>
2024-07-29 17:58:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "27500622"))
 API_HASH = getenv("API_HASH", "d45e39daf0e869ad0807311f14fb83fd")
-BOT_TOKEN = getenv("BOT_TOKEN", "6713397211:AAE5o9r-FamtWXTINNcxWWaCGeBuRVaLkrg")
+BOT_TOKEN = getenv("BOT_TOKEN", "6713397211:AAHO7sUwepCdwH2Wp-iCbz_kG9Zxoh2z8mY")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7008835401 6332124181").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://SUSHIL-SINGH:aIKAr5fTy2hH2zU8@sushil-singh.rop0dec.mongodb.net/?retryWrites=true&w=majority&appName=SUSHIL-SINGH")
 LOG_GROUP = getenv("LOG_GROUP", "-1002228957170")
