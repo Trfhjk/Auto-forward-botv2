90ced7b43fd2a134ce2a4007112840d490f92df3
Sushilgithubbot <sushilgithubbot@gmail.com>
2024-07-30 20:09:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "27500622"))
+API_HASH = getenv("API_HASH", "d45e39daf0e869ad0807311f14fb83fd")
+BOT_TOKEN = getenv("BOT_TOKEN", "6713397211:AAHO7sUwepCdwH2Wp-iCbz_kG9Zxoh2z8mY")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7008835401 6332124181").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://SUSHIL-SINGH:aIKAr5fTy2hH2zU8@sushil-singh.rop0dec.mongodb.net/?retryWrites=true&w=majority&appName=SUSHIL-SINGH")
+LOG_GROUP = getenv("LOG_GROUP", "-1002228957170")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002180994841"))
