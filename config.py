ae853ef6e609f96aa4693409661c94730ed8ef59
Sushilgithubbot <sushilgithubbot@gmail.com>
2024-07-30 19:36:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,10 @@
 
 from os import getenv
 
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
